f74a6188ab51a77c3364f6a50af6a701ce1e6fca
dirk teucher <dirkteucher@users.noreply.github.com>
2017-11-21 12:08:11 +0000

    Added vuetifyjs
    Added the vuetify material design framework to enable some out the box design for html elements, components and modules.
    
    1) npm install vuetify --save
    2) Add font file to index.html
    3) Add vuetify to vue and import css file in main.js
    4) Grap some components from the vuetify website and put them into the home.vue file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Router
+import router from './router/router.js';
+Vue.use(router);
 
-import router from './router/router.js'
-Vue.use(router)
+
+// Vue Material design
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import '../node_modules/vuetify/dist/vuetify.min.css';
+
 
 new Vue({
   el: '#app',
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
-      Home page
-         <router-link to="/secondPage">Second page</router-link>
+  <div
+    id="e3"
+    style="margin: auto;"
+    class="grey lighten-3"
+  >
+    <v-toolbar color="pink">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">My Music</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-container
+      fluid
+      style="min-height: 0;"
+      grid-list-lg
+    >
+      <v-layout row wrap>
+        
+        <v-flex xs12>
+          <v-card color="blue-grey darken-2" class="white--text">
+            <v-card-title primary-title>
+              <div class="headline">Unlimited music now</div>
+              <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat dark>Listen now</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+
+
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
