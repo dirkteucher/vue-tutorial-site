e96312538fe18b9b8578073bbeb0ebd7b17eebb8
dirk teucher <dirkteucher@users.noreply.github.com>
2017-11-22 15:29:10 +0000

    Default new page template
    This is the default template I am currently using for each new page that is created.

---
+++ src/pages/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+		<headermain></headermain>
+
+		<v-container grid-list-md >
+			<v-layout row wrap>
+				<v-flex xs12>
+					<h1> Default.</h1>
+					
+				</v-flex>
+			</v-layout>
+
+			<v-layout row wrap>
+				<v-flex xs5  >
+					<div>
+						<a href="https://github.com/dirkteucher/vue-tutorial-site/blob/master/src/pages/declarative-rendering.vue" target="_blank">
+							<v-btn color="green white--text">default</v-btn>
+						</a>
+
+					</div>
+				</v-flex>
+			</v-layout>
+
+		</v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'default',
+  data () {
+    return {
+	  message: ''
+    }
+  }
+}
+</script>
